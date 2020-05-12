f273e38b272823502fd3a0b49414456c90ed0317
TanishqSingla <tanishqsingla00@gmail.com>
2020-05-12 21:35:45 +0530

    Completed

--- app.js
+++ app.js
@@ -69,19 +69,26 @@ app.post("/compose", function (req, res) {
   res.redirect("/");
 });
 
-app.get("/posts/:postName", function (req, res) {
-  const requestedTitle = _.lowerCase(req.params.postName);
+app.get("/posts/:postId", function (req, res) {
+  const requestedPostId = req.params.postId;
 
-  posts.forEach(function (post) {
-    const storedTitle = _.lowerCase(post.title);
-
-    if (storedTitle === requestedTitle) {
-      res.render("post", {
-        title: post.title,
-        content: post.content,
-      });
-    }
+  Post.findOne({ _id: requestedPostId }, (e, post) => {
+    res.render("post", {
+      title: post.title,
+      content: post.content,
+    });
   });
+
+  // posts.forEach(function (post) {
+  //   const storedTitle = _.lowerCase(post.title);
+
+  //   if (storedTitle === requestedTitle) {
+  //     res.render("post", {
+  //       title: post.title,
+  //       content: post.content,
+  //     });
+  //   }
+  // });
 });
 
 app.listen(3000, function () {
